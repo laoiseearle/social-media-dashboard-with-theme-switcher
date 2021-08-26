6d6c0bf8a60ee17752a7acc3cc23267a53334045
Laoise Earle <laoiseearle92@gmail.com>
2021-08-27 00:00:34 +0100

    Style overview component

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import { React, useContext } from 'react';
 import { dailyOverview } from '../data';
+import { ThemeContext } from '../App';
+import { cardBackgroundColor, secondaryTextColor } from '../theme';
+
+import './overview.css';
 
 export const Overview = () => {
+  const { theme } = useContext(ThemeContext);
+  const getTheme = name => (theme === 'dark' ? name.dark : name.light);
+
   return (
     <section className="overview">
-      <h2>Overview - Today</h2>
+      <h2 className="overview-header" style={getTheme(secondaryTextColor)}>
+        Overview - Today
+      </h2>
 
       {dailyOverview.map(item => (
-        <div className="stat-info" key={item.id}>
-          <p>{item.infoType}</p>
+        <div
+          className="overview-card"
+          key={item.id}
+          style={getTheme(cardBackgroundColor)}
+        >
+          <div className="overview-card-top">
+            <p>{item.infoType}</p>
+            <img src={item.logo} alt="" />
+          </div>
 
-          <div className="stat-count">
-            <h2>{item.count}</h2>
-            <p>{item.dailyChange}</p>
+          <div className="overview-card-bottom">
+            <h2 style={getTheme(secondaryTextColor)}>{item.count}</h2>
+
+            <div
+              className="percentage"
+              style={{ color: item.increasedChange ? '#1db489' : '#dc414c' }}
+            >
+              <img
+                src={
+                  item.increasedChange
+                    ? './images/icon-up.svg'
+                    : './images/icon-down.svg'
+                }
+                alt=""
+              />
+              <p>{item.dailyChange}</p>
+            </div>
           </div>
         </div>
       ))}
