4671c773ff4fc7c3bc2203f34796dab59dc05492
Laoise Earle <laoiseearle92@gmail.com>
2021-08-26 21:34:18 +0100

    Create theme context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Header } from './Components/Header';
 import { Socials } from './Components/Socials';
 import { Overview } from './Components/Overview';
+import { backgroundColor } from './theme.js';
 import './app.css';
 
 export const ThemeContext = React.createContext();
@@ -10,12 +11,16 @@ export const ThemeContext = React.createContext();
 export const App = () => {
   const [theme, setTheme] = useState('dark');
 
+  const getTheme = name => (theme === 'dark' ? name.dark : name.light);
+
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div className="App">
-        <Header />
-        <Socials />
-        <Overview />
+      <div style={getTheme(backgroundColor)} className="App">
+        <div className="wrapper">
+          <Header />
+          <Socials />
+          <Overview />
+        </div>
       </div>
     </ThemeContext.Provider>
   );
--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../App';
 import { socialSites } from '../data';
+import { cardBackgroundColor, secondaryTextColor } from '../theme';
 import './socials.css';
 
 export const Socials = () => {
+  const { theme } = useContext(ThemeContext);
+
+  const getTheme = name => (theme === 'dark' ? name.dark : name.light);
+
   return (
     <section className="social-sites">
       {socialSites.map(site => (
-        <div className="site" key={site.id}>
-          {console.log(site.logo)}
-
+        <div
+          className="site"
+          key={site.id}
+          style={getTheme(cardBackgroundColor)}
+        >
           <div className="top-card">
             <img className="icon" src={site.logo} alt="" />
             <p>{site.username}</p>
           </div>
 
           <div className="center-card">
-            <h1>{site.total}</h1>
+            <h1 style={getTheme(secondaryTextColor)}>{site.total}</h1>
             <h3>{site.subName}</h3>
           </div>
 
