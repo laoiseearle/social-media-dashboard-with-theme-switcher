cf2f96bfb7eed1edf4a9d4b167cfab0893a4a1f2
Laoise Earle <laoiseearle92@gmail.com>
2021-08-26 12:22:08 +0100

    Style social cards

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import { socialSites } from '../data';
+import './socials.css';
 
 export const Socials = () => {
   return (
     <section className="social-sites">
       {socialSites.map(site => (
         <div className="site" key={site.id}>
-          <p>{site.username}</p>
-          <h1>{site.total}</h1>
-          <h3>{site.subName}</h3>
-          <p>{site.dailyChange}</p>
+          {console.log(site.logo)}
+
+          <div className="top-card">
+            <img className="icon" src={site.logo} alt="" />
+            <p>{site.username}</p>
+          </div>
+
+          <div className="center-card">
+            <h1>{site.total}</h1>
+            <h3>{site.subName}</h3>
+          </div>
+
+          <div
+            className="bottom-card"
+            style={{ color: site.increasedChange ? '#1db489' : '#dc414c' }}
+          >
+            <img
+              src={
+                site.increasedChange
+                  ? './images/icon-up.svg'
+                  : './images/icon-down.svg'
+              }
+              alt=""
+            />
+            <p>{site.dailyChange} Today</p>
+          </div>
         </div>
       ))}
     </section>
