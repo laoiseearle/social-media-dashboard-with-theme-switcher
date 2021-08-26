2602e9df3c4678e727b9ea12f000be42d17f2eab
Laoise Earle <laoiseearle92@gmail.com>
2021-08-26 21:27:29 +0100

    Create global theme

---
+++ src/theme.js
@@ -0,0 +1,28 @@
+export const backgroundColor = {
+  light: {
+    backgroundColor: '#ffffff',
+    color: '#63687e',
+  },
+  dark: {
+    backgroundColor: '#1e202a',
+    color: '#8b97c6',
+  },
+};
+
+export const cardBackgroundColor = {
+  light: {
+    backgroundColor: '#f0f2fa',
+  },
+  dark: {
+    backgroundColor: '#252a41',
+  },
+};
+
+export const secondaryTextColor = {
+  light: {
+    color: '#1e202a',
+  },
+  dark: {
+    color: '#ffffff',
+  },
+};
